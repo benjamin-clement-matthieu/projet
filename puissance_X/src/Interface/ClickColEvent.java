b865ccc4bc2ea1ca2c5cb44317b7b5b704dac291
benjamin-clement-matthieu <clement.ansel14@gmail.com>
2017-06-02 13:52:07 +0200

    ClickColEvent change

--- puissance_X/src/Interface/ClickColEvent.java
+++ puissance_X/src/Interface/ClickColEvent.java
@@ -13,22 +13,7 @@ public class ClickColEvent implements ActionListener {
 	
 	public void actionPerformed(ActionEvent arg0) {
 		JButton b = (JButton) arg0.getSource();
-		String bName = b.getName();
-		if(bName.equals("col1"))
-			selectedCol = 0;
-		else if(bName.equals("col2"))
-			selectedCol = 1;
-		else if(bName.equals("col3"))
-			selectedCol = 2;
-		else if(bName.equals("col4"))
-			selectedCol = 3;
-		else if(bName.equals("col5"))
-			selectedCol = 4;
-		else if(bName.equals("col6"))
-			selectedCol = 5;
-		else if(bName.equals("col7"))
-			selectedCol = 6;
-		
+		selectedCol = Integer.valueOf(b.getName());
 		clicked = true;
 	}
 	
